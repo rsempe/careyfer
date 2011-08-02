af3b814c9f422feb644cf28dcba001270fc7f8b6
Romain Sempé <romain.sempe@gmail.com>
2011-08-02 21:06:57 +0200

    Set title on each page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
+
+  before_filter :set_title
+
+  private
+
+  def set_title
+    @title = "CareyFer - Ferronerie de Jean Paul Carey"
+  end
 end
