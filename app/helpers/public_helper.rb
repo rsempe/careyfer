009a302673e4dc8feb3deeb47dd41658783c4c97
Romain Sempé <romain.sempe@gmail.com>
2011-08-03 19:43:02 +0200

    Add some improvements.

--- app/helpers/public_helper.rb
+++ app/helpers/public_helper.rb
@@ -2,4 +2,9 @@ module PublicHelper
   def is_active?(page_name)
     "active" if params[:action] == page_name
   end
+
+  def project_pictures
+    pictures = Dir.foreach("public/images/projects").inject([]) { |sum, picture| sum << picture }
+    pictures.delete_if { |file| File.directory?(file) }
+  end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,42 +10,4 @@ document.observe("dom:loaded", function() {
       position: mapLatlng, map: map, title: "Ferronerie CareyFer"
     });
   }
-
-  // Projects slideshow
-
 });
-
-var image_slide = new Array('project1', 'project2', 'project3');
-var i = 0;
-
-function SwapImage(x,y) {
-  $(image_slide[x]).appear({ duration: 1.4 });
-  $(image_slide[y]).fade({ duration: 0 });
-}
-
-function GoNext() {
-  var imageShow = i+1,
-      imageHide = i;
-
-  if (imageShow == image_slide.length) {
-    SwapImage(0,imageHide);
-    i = 0;
-  } else {
-    SwapImage(imageShow,imageHide);
-    i++;
-  }
-}
-
-function GoPrevious() {
-  var imageShow = i-1,
-      imageHide = i;
-
-  if (i == 0) {
-    SwapImage(NumOfImages-1,imageHide);
-    i = NumOfImages-1;
-
-  } else {
-    SwapImage(imageShow,imageHide);
-    i--;
-  }
-}
